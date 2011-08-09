ff3bc2b4cc64084c43ff9da776a67faaeb2e883b
Raymond Chandler III <raymond.chandler@etech.ohio.gov>
2011-08-09 11:54:53 -0400

    Added bulk modification of atomic learning status on JVSDs and School Districts

--- gisedu/admin.py
+++ gisedu/admin.py
@@ -13,6 +13,26 @@ class OhioSchoolDistrictsAdmin(admin.GeoModelAdmin):
     exclude = ['gid', 'id']
 
     list_filter = ('comcast_coverage', 'has_atomic_learning')
+    actions = ['atomic_learning_true', 'atomic_learning_false']
+
+    def atomic_learning_true(self, request, queryset):
+        rows_updated = queryset.update(has_atomic_learning=True)
+        if rows_updated == 1:
+            message_bit = "1 School District was"
+        else:
+            message_bit = "%s School Districts were" % rows_updated
+        self.message_user(request, "%s successfully marked as participants in atomic learning." % message_bit)
+
+    def atomic_learning_false(self, request, queryset):
+        rows_updated = queryset.update(has_atomic_learning=False)
+        if rows_updated == 1:
+            message_bit = "1 School District was"
+        else:
+            message_bit = "%s School Districts were" % rows_updated
+        self.message_user(request, "%s successfully un-marked as participants in atomic learning." % message_bit)
+
+    atomic_learning_true.short_description = "Mark selected School Districts as participants in atomic learning."
+    atomic_learning_false.short_description = "Un-mark selected School Districts as participants in atomic learning."
 
 class OhioHouseDistrictsAdmin(admin.GeoModelAdmin):
     exclude = ['gid']
--- jvsd/admin.py
+++ jvsd/admin.py
@@ -8,5 +8,25 @@ class GiseduJointVocationalSchoolDistrictAdmin(gis_admin.GeoModelAdmin):
     exclude = ['gid', 'org_key']
 
     list_filter = ('has_atomic_learning', )
+    actions = ['atomic_learning_true', 'atomic_learning_false']
+
+    def atomic_learning_true(self, request, queryset):
+        rows_updated = queryset.update(has_atomic_learning=True)
+        if rows_updated == 1:
+            message_bit = "1 JVSD was"
+        else:
+            message_bit = "%s JVSDs were" % rows_updated
+        self.message_user(request, "%s successfully marked as participants in atomic learning." % message_bit)
+
+    def atomic_learning_false(self, request, queryset):
+        rows_updated = queryset.update(has_atomic_learning=False)
+        if rows_updated == 1:
+            message_bit = "1 JVSD was"
+        else:
+            message_bit = "%s JVSDs were" % rows_updated
+        self.message_user(request, "%s successfully un-marked as participants in atomic learning." % message_bit)
+
+    atomic_learning_true.short_description = "Mark selected JVSDs as participants in atomic learning."
+    atomic_learning_false.short_description = "Un-mark selected JVSDs as participants in atomic learning."
 
 admin.site.register(GiseduJointVocationalSchoolDistrict, GiseduJointVocationalSchoolDistrictAdmin)
